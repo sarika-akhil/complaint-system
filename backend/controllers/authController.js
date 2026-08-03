c0f942cd5fa4f67af9a44657786b7f68a3683d29
akhil-1-bit <akhilsarika0@gmail.com>
2026-08-03 21:13:35 +0530

    Force IPv4 SMTP

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,11 @@ const nodemailer = require('nodemailer');
 const otpStore = new Map();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
